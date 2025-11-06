40ca02caf1d3ede1fa5ad70b22ef91b255f6fb3e
cyberneftest-svg <cyberneftest@gmail.com>
2025-11-06 22:07:59 +0500

    feedback added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
             <input type="text" placeholder="Name" className="mb-2 p-2 text-lg border border-gray-300 rounded" />
             <input type="email" placeholder="Email" className="mb-2 p-2 text-lg border border-gray-300 rounded" />
             <input type="password" placeholder="Password" className="mb-4 p-2 text-lg border border-gray-300 rounded" />
-            <button type="submit" className="p-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600 hover:cursor-pointer" onClick={() => window.location.href = '/login'}>Register</button>
+            <button type="submit" className="p-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600 hover:cursor-pointer">Register</button>
         </form>
         <p className='mt-4'>Already have an account? <a href="/login" className='text-blue-300 hover:text-blue-600'>Login</a></p>
     </div>
